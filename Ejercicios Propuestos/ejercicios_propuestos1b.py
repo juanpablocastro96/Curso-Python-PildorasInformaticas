a1d484faae780c423b360c433bd4449f0c081e14
juanpablocastro96 <48337267+juanpablocastro96@users.noreply.github.com>
2023-05-06 09:03:03 -0500

    Update ejercicios_propuestos1b.py
    Fixing return statement within the function.

--- Ejercicios Propuestos/ejercicios_propuestos1b.py
+++ Ejercicios Propuestos/ejercicios_propuestos1b.py
@@ -1,6 +1,7 @@
 a = int(input("Ingrese el primero número que quiere sumar: "))
 b = int(input("Ingrese el segundo número que quiere sumar: "))
 c = int(input("Ingrese el tercer número que quiere sumar: "))
+
 def sumaAritmetica(n,m,p):
-	return a + b + c
+	return n + m + p
 print("El resultado es: " +str(sumaAritmetica(a,b,c)))
